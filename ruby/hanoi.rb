4da91188fc792488591489a45c5e788cd637ce50
Roberto Efrain Uribe Capulin <euribester@gmail.com>
2016-08-29 13:21:23 -0500

    Programa para resolver la torre de hanoi en Ruby

---
+++ ruby/hanoi.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+=begin
+	 Abordamos el problema de las torres de Hanoi con recursividad
+=end
+
+def solve_hanoi(disco, desde, hasta, aux, &block)
+	if disco == 1 
+		yield(disco, desde, hasta)
+		return
+	end
+	solve_hanoi(disco-1, desde, aux, hasta, &block)
+	yield(disco, desde, hasta)
+	solve_hanoi(disco-1, aux, hasta, desde, &block)
+end
+
+solve_hanoi(ARGV[0].to_i, 'A', 'C', 'B') { |disco, desde, hasta| puts "Mover #{disco} desde #{desde} hasta #{hasta}"}
