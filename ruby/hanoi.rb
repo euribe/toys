610987ec7c272f25015d32286da8a98cafc2e2a6
Roberto Efrain Uribe Capulin <euribester@gmail.com>
2016-08-29 17:10:46 -0500

    Validamos la cantidad de argumentos que recibimos

--- ruby/hanoi.rb
+++ ruby/hanoi.rb
@@ -14,4 +14,9 @@ def solve_hanoi(disco, desde, hasta, aux, &block)
 	solve_hanoi(disco-1, aux, hasta, desde, &block)
 end
 
+if ARGV.count != 1
+	puts "Se necesita un argumento numerico (La cantidad de discos)"
+	exit
+end
+
 solve_hanoi(ARGV[0].to_i, 'A', 'C', 'B') { |disco, desde, hasta| puts "Mover #{disco} desde #{desde} hasta #{hasta}"}
